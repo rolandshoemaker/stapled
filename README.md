a7ed3a604ec4dfecd5e154c6a7825c080b1a42d8
Roland Shoemaker <rolandshoemaker@gmail.com>
2016-02-13 21:40:34 -0800

    Add License and basic README

---
+++ README.md
@@ -0,0 +1,5 @@
+# `stapled`
+
+A caching OCSP daemon that makes stapling less painful. Inspired in
+large part by the [notes](https://gist.github.com/sleevi/5efe9ef98961ecfb4da8)
+written on the topic by Ryan Sleevi.
--- ocsp.go
+++ ocsp.go
@@ -6,21 +6,34 @@ package stapled
 import (
 	"bytes"
 	"crypto/x509"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"math/big"
 	mrand "math/rand"
+	"net/http"
+	"strconv"
 	"sync"
 	"time"
 
 	"golang.org/x/crypto/ocsp"
+	"golang.org/x/net/context"
 )
 
 type entry struct {
+	serial *big.Int
 	issuer *x509.Certificate
 
 	responder        string
+	client           *http.Client
+	deadline         time.Duration
+	request          []byte
 	overrideUpstream bool
 
 	lastSync    time.Time
 	maxAge      time.Duration
+	eTag        string
 	response    []byte
 	nextPublish time.Time
 	nextUpdate  time.Time
@@ -37,20 +50,87 @@ func entryFromDefinition() (*entry, error) {
 	return nil, nil
 }
 
-func (e *entry) verifyResponse(respBytes []byte) (*ocsp.Response, error) {
-	return nil, nil
-}
-
-func (e *entry) loadResponseFromFile(cacheFolder string) error {
+func (e *entry) verifyResponse(resp *ocsp.Response) error {
+	if resp.ThisUpdate.After(time.Now()) {
+		return errors.New("Malformed OCSP response: ThisUpdate is in the future")
+	}
+	if resp.ThisUpdate.After(resp.NextUpdate) {
+		return errors.New("Malformed OCSP response: NextUpdate is before ThisUpate")
+	}
+	if err := resp.CheckSignatureFrom(e.issuer); err != nil {
+		return err
+	}
+	if e.serial.Cmp(resp.SerialNumber) != 0 {
+		return errors.New("Malformed OCSP response: Serial numbers don't match")
+	}
+	// check signing cert is still valid? (this could
+	// probably also be taken care of somewhere else...)
 	return nil
 }
 
-func (e *entry) fetchResponse() ([]byte, error) {
-	return nil, nil
+func (e *entry) fetchResponse(ctx context.Context) (*ocsp.Response, []byte, error) {
+	backoffSeconds := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, nil, ctx.Err()
+		case <-time.NewTimer(time.Duration(backoffSeconds) * time.Second).C:
+		}
+		req, err := http.NewRequest(
+			"GET",
+			fmt.Sprintf(
+				"%s/%s",
+				e.responder,
+				base64.StdEncoding.EncodeToString(e.request),
+			),
+			nil,
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		if e.eTag != "" {
+			req.Header.Set("If-None-Match", e.eTag)
+		}
+		resp, err := e.client.Do(req)
+		if err != nil {
+			// log
+			backoffSeconds = 10
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != 200 {
+			backoffSeconds = 10
+			if resp.StatusCode == 503 {
+				if retryAfter := resp.Header.Get("Retry-After"); retryAfter != "" {
+					if seconds, err := strconv.Atoi(retryAfter); err == nil {
+						backoffSeconds = seconds
+					}
+				}
+			}
+			continue
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			// log
+			backoffSeconds = 10
+			continue
+		}
+		ocspResp, err := ocsp.ParseResponse(body, e.issuer)
+		if err != nil {
+			// log
+			backoffSeconds = 10
+			continue
+		}
+		if ocspResp.Status == int(ocsp.Success) {
+			return ocspResp, body, nil
+		}
+		backoffSeconds = 10
+	}
 }
 
 func (e *entry) updateResponse() error {
-	respBytes, err := e.fetchResponse()
+	ctx := context.WithTimeout(context.Background(), e.deadline)
+	resp, respBytes, err := e.fetchResponse(ctx)
 	if err != nil {
 		return err
 	}
@@ -59,7 +139,7 @@ func (e *entry) updateResponse() error {
 		e.lastSync = now
 		return nil
 	}
-	resp, err := e.verifyResponse(respBytes)
+	err = e.verifyResponse(resp)
 	if err != nil {
 		return err
 	}
