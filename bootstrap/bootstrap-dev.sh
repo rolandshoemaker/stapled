f7f4e40e38f5caefff0ae88efe9fa717b12e5942
Daniel <daniel@binaryparadox.net>
2016-03-06 15:58:45 -0500

    Adds Vagrant support.
    This commit adds a Vagrantfile to support having a VM based dev env.
    
    I've selected Ubuntu 14.04 (Trusty) as the base image, but I'm happy to
    change that if anyone has preferences. I think it's a good common
    denominator OS. Currently the VM is provisioned with 2GB of RAM
    
    The Go version being installed is fixed at 1.5.3 and there is
    a hardcoded SHA1 sum in the provisioning script that the release
    `.tar.gz` is compared against. If there's demand for using `1.6.0` as
    the default I'm happy to switch this out.
    
    Two bootstrap scripts are run during provisioning:
    
    1. `bootstrap/bootstrap-dev-admin.sh` - invoked as root in the VM
    2. `bootstrap/bootstrap-dev.sh` - invoked as `vagrant` user in the VM
    
    The first is primarily used to install Go and set up system wide
    settings.
    
    The second sets up a Go workspace, adds it to the `vagrant` user's
    environment, and links the project src from the host machine into the
    VM's gopath.
    
    Workflow:
    
    1.`vagrant up`
    2. modify files in your local git checkout on your host machine
    3. `vagrant ssh`
    4. `cd workspace/src/github.com/rolandshoemaker/stapled`
    5. `go test ./...`
    6. etc.

---
+++ bootstrap/bootstrap-dev.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# boostrap-dev.sh is run as the `vagrant` user by Vagrant (see Vagrantfile) during
+# provisioning. It is the non-root half of bootstrap-dev-admin.sh
+#
+# To make it convenient to develop on the host machine the `/vagrant` mount
+# shared between the host and the Vagrant VM is linked directly into the Gopath
+# on the VM. Edit locally, `vagrant ssh`, `cd` to the src directory in `~/workspace`,
+# and run `go test ./...` to test local work.
+#
+# Responsibilities:
+#   - Creating a $GOPATH
+#   - Adding $GOPATH to the environment
+#   - Adding $GOPATH's bin/ to the $PATH
+#   - Linking the Vagrant host's mountpoint into the $GOPATH
+#   - Installing godep
+#   - Vetting the stapled src, running tests, and installing it.
+
+set -e
+
+GOPATH=$HOME/workspace
+PROJECT_ROOT=$GOPATH/src/github.com/rolandshoemaker
+PROJECT=$PROJECT_ROOT/stapled
+
+echo "Creating GOPATH in $GOPATH"
+mkdir -p "$GOPATH"
+
+echo "Setting GOPATH"
+export GOPATH
+echo 'export GOPATH=$HOME/workspace' >> ~/.bashrc
+
+echo "Adding GOPATH bin/ to PATH"
+export PATH=$PATH:$GOPATH/bin
+echo 'export PATH=$PATH:$HOME/workspace/bin' >> ~/.bashrc
+
+echo "Enabling GO15VENDOREXPERIMENT"
+export GO15VENDOREXPERIMENT=1
+echo 'export GO15VENDOREXPERIMENT=1' >> ~/.bashrc
+
+echo "Creating project structure in GOPATH"
+mkdir -p "$PROJECT_ROOT"
+
+echo "Linking project src to GOPATH"
+ln -s /vagrant/ "$PROJECT"
+
+echo "Installing godep"
+go get github.com/tools/godep
+
+pushd "$PROJECT"
+  echo "Installing project"
+
+  # Uncomment when vetting passes.
+  #godep go vet ./...
+
+  go test -v ./...
+  go install ./...
+popd
+
+echo "Finished bootstrap-dev.sh"
